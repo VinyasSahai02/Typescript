0c65ec36311c8e7d1b7ec0c64c2fe30a521472b7
vinyas <vinyassahai03@gmail.com>
2024-07-15 20:53:21 +0530

    introduction to typescript

---
+++ Basics/01_intro.ts
@@ -0,0 +1,13 @@
+//ts is a development tool like a wrapper around js
+//the project still runs in js
+//ts gets converted to js-->TypeScript is transpiled into JavaScript using a compiler.
+
+//ts checks if the specified type matches before running the code
+
+const user={name:"vinyas",age:21}
+console.log("Sahai");
+// console.log(user.email);
+export{}
+
+//diff types include--> any,unknown,object,void,undefined,null,never
+// see docs for better understanding
